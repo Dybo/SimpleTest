e83df850d1c3780bc972d3b3ae2faf08c5dd6a85
Dylan Boertje <dylanboertje@gmail.com>
2015-12-23 21:10:07 +0100

    coveralls

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -40,6 +40,20 @@ class ComposerAutoloaderInit10d69e46a1a50e027a5904ee54896351
 
         $loader->register(true);
 
+        $includeFiles = require __DIR__ . '/autoload_files.php';
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire10d69e46a1a50e027a5904ee54896351($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire10d69e46a1a50e027a5904ee54896351($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
